45792de341d15f775ab58236ff7dae0ba0cbba87
Guilherme Teixeira <guiemi.teixeira@gmail.com>
2020-12-30 14:09:41 -0300

    Format code

--- src/App.test.js
+++ src/App.test.js
@@ -1,37 +1,36 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 
-import App, { calcularNovoSaldo } from './app';
+import App, { calcularNovoSaldo } from "./app";
 
-describe('Componente principal', () => {
-  describe('Quando eu abro o app do banco', () => {
-    test('o nome é exibido', () => {
+describe("Componente principal", () => {
+  describe("Quando eu abro o app do banco", () => {
+    test("o nome é exibido", () => {
       render(<App />);
-  
-      expect(screen.getByText('ByteBank')).toBeInTheDocument();
-    })
-    it('o saldo é exibido', () => {
+
+      expect(screen.getByText("ByteBank")).toBeInTheDocument();
+    });
+    it("o saldo é exibido", () => {
       render(<App />);
-    
-      expect(screen.getByText('Saldo:')).toBeInTheDocument();
-    })
-    it('o botão de realizar transação é exibido', () => {
+
+      expect(screen.getByText("Saldo:")).toBeInTheDocument();
+    });
+    it("o botão de realizar transação é exibido", () => {
       render(<App />);
-  
-      expect(screen.getByText('Realizar operação')).toBeInTheDocument();
-    })
-  })
-  describe('Quando eu realizo uma transação', () => {
-    it('que é um saque, o valor vai diminuir', () => {
+
+      expect(screen.getByText("Realizar operação")).toBeInTheDocument();
+    });
+  });
+  describe("Quando eu realizo uma transação", () => {
+    it("que é um saque, o valor vai diminuir", () => {
       const valores = {
-        transacao: 'saque',
-        valor: 50
-      }
+        transacao: "saque",
+        valor: 50,
+      };
 
-      const novoSaldo = calcularNovoSaldo(valores, 150)
+      const novoSaldo = calcularNovoSaldo(valores, 150);
 
       expect(novoSaldo).toBe(100);
     });
-  })
-})
-
+  });
+});
